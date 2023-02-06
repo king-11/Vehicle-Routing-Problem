a8e4f6dd27dcc1a12e852f17ab9dce1cff8b5df7
atharvabhatt200 <atharvabhatt200@gmail.com>
2023-02-06 19:43:40 +0530

    add a auxiliary function for mutation

--- src/route_mutation.rs
+++ src/route_mutation.rs
@@ -17,3 +17,14 @@ pub fn route_mutate(routes: &mut Vec<Route>, distance_matrix: &Vec<Vec<f32>>, ti
     clustering(node, routes, distance_matrix, time_matrix, rider_matrix);
   }
 }
+
+pub fn mutate_aux(routes: &mut Vec<Route>, distance_matrix: &Vec<Vec<f32>>, time_matrix: &Vec<Vec<f32>>, rider_matrix: &Vec<Vec<f32>>, nodes_mut: usize) {
+  let init_cost = routes.iter().map(|route| route.calc_distance(distance_matrix)).collect_vec();
+  
+  let mut mutated_routes = routes.clone();
+  route_mutate(&mut mutated_routes, distance_matrix, time_matrix, rider_matrix, nodes_mut);
+  let new_cost = mutated_routes.iter().map(|route| route.calc_distance(distance_matrix)).collect_vec();
+  if new_cost < init_cost {
+    *routes = mutated_routes.clone();
+  }
+}
