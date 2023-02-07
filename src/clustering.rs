2e97ac3f5863c4f51433d7679b46f747fad1e55d
atharvabhatt200 <atharvabhatt200@gmail.com>
2023-02-07 20:44:06 +0530

    implement clustring and mutation function for js

--- src/clustering.rs
+++ src/clustering.rs
@@ -6,29 +6,78 @@ use super::genetic_algorithm::genetic_algorithm;
 
 use super::model::*;
 
-pub fn clustering(node: Node, routes: &mut Vec<Route> ,distance_matrix: &Vec<Vec<f32>>, time_matrix: &Vec<Vec<f32>>, rider_matrix: &Vec<Vec<f32>>) {
-	// most priority to distance travelled, add ratio too?
-	let mut min_increase = MAX;
-	let mut min_rider_index = 0;
-	let mut optimal_rider = routes[0].clone();
+use serde_wasm_bindgen::*;
 
-  for (rider_index, route) in routes.iter().enumerate() {
-    let mut temp_route = route.clone();
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
-    // does it matter of GA to have input in a particular order?
-    temp_route.nodes.push(node);
-    let (cost, new_route) = genetic_algorithm(distance_matrix, time_matrix, rider_matrix, rider_index, &temp_route);
+#[wasm_bindgen]
+pub fn invoke_clustering_from_js(
+    routes: JsValue,
+    node: JsValue,
+    distance_matrix: JsValue,
+    time_matrix: JsValue,
+    rider_matrix: JsValue,
+) -> JsValue {
+    let mut routes: Vec<Route> = serde_wasm_bindgen::from_value(routes).unwrap();
+    let node: Node = serde_wasm_bindgen::from_value(node).unwrap();
+    let distance_matrix: Vec<Vec<f32>> = serde_wasm_bindgen::from_value(distance_matrix).unwrap();
+    let time_matrix: Vec<Vec<f32>> = serde_wasm_bindgen::from_value(time_matrix).unwrap();
+    let rider_matrix: Vec<Vec<f32>> = serde_wasm_bindgen::from_value(rider_matrix).unwrap();
+    clustering(
+        node,
+        &mut routes,
+        &distance_matrix,
+        &time_matrix,
+        &rider_matrix,
+    );
+    return serde_wasm_bindgen::to_value(&routes).unwrap();
+}
 
-    let nodes = new_route.iter().map(|&idx| temp_route.nodes.iter().find(|node| node.index == idx).unwrap().clone()).collect_vec();
-    temp_route.nodes = nodes;
+pub fn clustering(
+    node: Node,
+    routes: &mut Vec<Route>,
+    distance_matrix: &Vec<Vec<f32>>,
+    time_matrix: &Vec<Vec<f32>>,
+    rider_matrix: &Vec<Vec<f32>>,
+) {
+    // most priority to distance travelled, add ratio too?
+    let mut min_increase = MAX;
+    let mut min_rider_index = 0;
+    let mut optimal_rider = routes[0].clone();
 
-    let increased_cost = cost - route.calc_distance(distance_matrix);
-    if increased_cost < min_increase {
-      min_increase = increased_cost;
-      min_rider_index = rider_index;
-      optimal_rider = temp_route;
+    for (rider_index, route) in routes.iter().enumerate() {
+        let mut temp_route = route.clone();
+
+        // does it matter of GA to have input in a particular order?
+        temp_route.nodes.push(node);
+        let (cost, new_route) = genetic_algorithm(
+            distance_matrix,
+            time_matrix,
+            rider_matrix,
+            rider_index,
+            &temp_route,
+        );
+
+        let nodes = new_route
+            .iter()
+            .map(|&idx| {
+                temp_route
+                    .nodes
+                    .iter()
+                    .find(|node| node.index == idx)
+                    .unwrap()
+                    .clone()
+            })
+            .collect_vec();
+        temp_route.nodes = nodes;
+
+        let increased_cost = cost - route.calc_distance(distance_matrix);
+        if increased_cost < min_increase {
+            min_increase = increased_cost;
+            min_rider_index = rider_index;
+            optimal_rider = temp_route;
+        }
     }
-  }
 
-  routes[min_rider_index] = optimal_rider;
+    routes[min_rider_index] = optimal_rider;
 }
--- src/route_mutation.rs
+++ src/route_mutation.rs
@@ -1,30 +1,95 @@
 use itertools::Itertools;
 
-use crate::{model::Route, node_selection::radial_selection, clustering::clustering};
+use crate::{clustering::clustering, model::Route, node_selection::radial_selection};
 
-pub fn route_mutate(routes: &mut Vec<Route>, distance_matrix: &Vec<Vec<f32>>, time_matrix: &Vec<Vec<f32>>, rider_matrix: &Vec<Vec<f32>>, nodes_mut: usize) {
-  let selected_nodes = radial_selection(&routes, distance_matrix, nodes_mut, Some(42)).iter().map(|&node| node.clone()).collect_vec();
+use serde_wasm_bindgen::*;
 
-  for route in routes.iter_mut() {
-    for node in &selected_nodes {
-      if let Some((idx, _)) = route.nodes.iter().find_position(|node_rem| node_rem.index == node.index) {
-        route.nodes.remove(idx);
-      }
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+
+pub fn route_mutate(
+    routes: &mut Vec<Route>,
+    distance_matrix: &Vec<Vec<f32>>,
+    time_matrix: &Vec<Vec<f32>>,
+    rider_matrix: &Vec<Vec<f32>>,
+    nodes_mut: usize,
+) {
+    let selected_nodes = radial_selection(&routes, distance_matrix, nodes_mut, Some(42))
+        .iter()
+        .map(|&node| node.clone())
+        .collect_vec();
+
+    for route in routes.iter_mut() {
+        for node in &selected_nodes {
+            if let Some((idx, _)) = route
+                .nodes
+                .iter()
+                .find_position(|node_rem| node_rem.index == node.index)
+            {
+                route.nodes.remove(idx);
+            }
+        }
     }
-  }
 
-  for node in selected_nodes {
-    clustering(node, routes, distance_matrix, time_matrix, rider_matrix);
-  }
+    for node in selected_nodes {
+        clustering(node, routes, distance_matrix, time_matrix, rider_matrix);
+    }
 }
 
-pub fn mutate_aux(routes: &mut Vec<Route>, distance_matrix: &Vec<Vec<f32>>, time_matrix: &Vec<Vec<f32>>, rider_matrix: &Vec<Vec<f32>>, nodes_mut: usize) {
-  let init_cost = routes.iter().map(|route| route.calc_distance(distance_matrix)).collect_vec();
-  
-  let mut mutated_routes = routes.clone();
-  route_mutate(&mut mutated_routes, distance_matrix, time_matrix, rider_matrix, nodes_mut);
-  let new_cost = mutated_routes.iter().map(|route| route.calc_distance(distance_matrix)).collect_vec();
-  if new_cost < init_cost {
-    *routes = mutated_routes.clone();
-  }
+pub fn mutate_aux(
+    iterations: usize,
+    routes: &mut Vec<Route>,
+    distance_matrix: &Vec<Vec<f32>>,
+    time_matrix: &Vec<Vec<f32>>,
+    rider_matrix: &Vec<Vec<f32>>,
+    nodes_mut: usize,
+) {
+    let init_cost = routes
+        .iter()
+        .map(|route| route.calc_distance(distance_matrix))
+        .collect_vec();
+
+    let best_cost = init_cost;
+
+    let mut mutated_routes = routes.clone();
+
+    for i in 0..iterations {
+        route_mutate(
+            &mut mutated_routes,
+            distance_matrix,
+            time_matrix,
+            rider_matrix,
+            nodes_mut,
+        );
+        let new_cost = mutated_routes
+            .iter()
+            .map(|route| route.calc_distance(distance_matrix))
+            .collect_vec();
+        if new_cost < best_cost {
+            *routes = mutated_routes.clone();
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn invoke_mutation_from_js(
+    iterations: usize,
+    routes: JsValue,
+    distance_matrix: JsValue,
+    time_matrix: JsValue,
+    rider_matrix: JsValue,
+    nodes_mut: usize,
+) -> JsValue {
+    let mut routes: Vec<Route> = serde_wasm_bindgen::from_value(routes).unwrap();
+    let distance_matrix: Vec<Vec<f32>> = serde_wasm_bindgen::from_value(distance_matrix).unwrap();
+    let time_matrix: Vec<Vec<f32>> = serde_wasm_bindgen::from_value(time_matrix).unwrap();
+    let rider_matrix: Vec<Vec<f32>> = serde_wasm_bindgen::from_value(rider_matrix).unwrap();
+    mutate_aux(
+        iterations,
+        &mut routes,
+        &distance_matrix,
+        &time_matrix,
+        &rider_matrix,
+        nodes_mut,
+    );
+    return serde_wasm_bindgen::to_value(&routes).unwrap();
 }
